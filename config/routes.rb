5e4b6448d2849296a38dce8a9f5908a0514fc724
priscilleumurerwa <priscille.umurerwa@gmail.com>
2019-09-23 22:40:43 +0000

    step 9 correction

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :users
   resources :users, only: [:new, :create, :show]
   resources :tasks
   root to:'tasks#index'
