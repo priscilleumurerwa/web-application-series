711be129c1976b32efa1900d220852da2f18710c
priscilleumurerwa <priscille.umurerwa@gmail.com>
2019-09-24 22:49:38 +0000

    step 12 correction

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   resources :users
   resources :users, only: [:new, :create, :show]
   resources :tasks
-  root to:'tasks#index'
+  root to:'sessions#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
