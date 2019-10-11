b84333c63277563076995d58bb46974759da7ec6
priscilleumurerwa <priscille.umurerwa@gmail.com>
2019-10-11 18:28:23 +0000

    step 20 login and logout function

--- db/schema.rb
+++ db/schema.rb
@@ -10,27 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_04_161418) do
+ActiveRecord::Schema.define(version: 2019_10_03_160531) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "models", force: :cascade do |t|
-    t.string "class"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
   create_table "tasks", force: :cascade do |t|
     t.string "name", null: false
     t.text "content", null: false
     t.string "status", null: false
+    t.integer "priority", null: false
     t.date "beginning_date", null: false
     t.date "ending_date", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
-    t.integer "priority"
     t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
@@ -40,7 +34,9 @@ ActiveRecord::Schema.define(version: 2019_10_04_161418) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password"
     t.string "password_digest"
+    t.boolean "admin"
     t.string "user_type"
   end
 
