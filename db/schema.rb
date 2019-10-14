731a6ad52f7f215d815134203dd88306aa2c42de
priscilleumurerwa <priscille.umurerwa@gmail.com>
2019-10-14 20:43:09 +0000

    step 19 creation of user with seed,login and logout links

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_03_160531) do
+ActiveRecord::Schema.define(version: 2019_10_14_201550) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,13 +30,10 @@ ActiveRecord::Schema.define(version: 2019_10_03_160531) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.string "type"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "password"
     t.string "password_digest"
-    t.boolean "admin"
     t.string "user_type"
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+User.create!(name: 'priscille', email: 'umurerwa@gmail.com',user_type: 'admin', password: "123456",password_confirmation: "123456")
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
